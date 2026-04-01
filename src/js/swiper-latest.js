050a753cf9769c5afb7f40d7e89b246be2da3daf
MaksymSelezen <foqney@gmail.com>
2026-04-01 18:44:51 +0300

    fix swiper + PP

--- src/js/swiper-latest.js
+++ src/js/swiper-latest.js
@@ -2,32 +2,29 @@ import Swiper from "swiper";
 import "swiper/css";
 
 const latestSliderSelector = ".js-blog-post-latest-swiper";
-const mobileMedia = window.matchMedia("(max-width: 767.98px)");
-
-let latestSwiper = null;
 
 const initLatestSwiper = () => {
   const latestSlider = document.querySelector(latestSliderSelector);
 
   if (!latestSlider) return;
 
-  if (mobileMedia.matches && !latestSwiper) {
-    latestSwiper = new Swiper(latestSlider, {
-      loop: true,
-      slidesPerView: 1.2,
-      spaceBetween: 26,
-      loopAdditionalSlides: 1,
-      speed: 500,
-      grabCursor: true,
-      watchOverflow: true,
-    });
-  }
+  const latestSwiper = new Swiper(latestSlider, {
+    loop: true,
+    loopAdditionalSlides: 1,
+    speed: 500,
+    grabCursor: true,
+    watchOverflow: true,
+    slidesPerView: 1.2,
+    spaceBetween: 26,
+    breakpoints: {
+      768: {
+        slidesPerView: 1.8,
+        spaceBetween: 20,
+      },
+    },
+  });
 
-  if (!mobileMedia.matches && latestSwiper) {
-    latestSwiper.destroy(true, true);
-    latestSwiper = null;
-  }
+  return latestSwiper;
 };
 
 initLatestSwiper();
-mobileMedia.addEventListener("change", initLatestSwiper);
